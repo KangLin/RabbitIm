a820ef71bd2aa079cc4e2e5cb4bd11dbff5f38d8
KangLin <kl222@126.com>
2014-04-13 23:03:08 +0800

    增加摄像头捕获、显示测试代码

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "MainWindow.h"
 #include <QApplication>
 #include <QTranslator>
+#include <QCamera>
+#include "Widgets/FrmVideo/CaptureVideoFrame.h"
+#include "Widgets/FrmVideo/FrmPlayer.h"
+#include <QVideoProbe>
 #include "Tool.h"
 
 int main(int argc, char *argv[])
@@ -21,8 +25,31 @@ int main(int argc, char *argv[])
     myappTranslator.load("app_" + locale, a.applicationDirPath());
     a.installTranslator(&myappTranslator);
 
+    /*
     MainWindow w;
-    w.show();
+    w.show();//*/
+
+    //*以下为视频捕获、显示测试代码
+    QList<QByteArray> device = QCamera::availableDevices();
+    QList<QByteArray>::iterator it;
+    for(it = device.begin(); it != device.end(); it++)
+    {
+        qDebug("Camera:%s", qPrintable(QCamera::deviceDescription(*it)));
+    }
+
+    QCamera camera;
+    CCaptureVideoFrame captureVideoFrame;
+    camera.setCaptureMode(QCamera::CaptureVideo);
+    CFrmPlayer player;
+    if(captureVideoFrame.setSource(&camera))
+    {
+        player.connect(&captureVideoFrame, SIGNAL(CaptureFrame(QVideoFrame)),
+                       SLOT(present(QVideoFrame)));
+    }
+
+    player.show();
+    player.activateWindow();
+    camera.start();//*/
 
     return a.exec();
 }
