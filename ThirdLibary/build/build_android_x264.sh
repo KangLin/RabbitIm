9ed61378fb3b8a519f88f342eb03a8e5f7a7644c
KangLin <kl222@126.com>
2014-06-19 12:09:04 +0800

    修改第三库编译脚本

--- ThirdLibary/build/build_android_x264.sh
+++ ThirdLibary/build/build_android_x264.sh
@@ -1,4 +1,4 @@
-NDK=$(ANDROID_NDK)               #指定android ndk根目录
+NDK=$(ANDROID_NDK_ROOT)               #指定android ndk根目录
 PREFIX=`pwd`/../android/         #修改这里为安装前缀
 NDK=/e/source/android/android-ndk-r9
 PREBUILT=$NDK/toolchains/arm-linux-androideabi-4.8/prebuilt
