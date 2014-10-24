e1b03d309a3dcd6ca945d9e2b840f880796dad3c
KangLin <kl222@126.com>
2014-10-24 13:39:10 +0800

    从opencv mat到 QVideoFrame 的二种转换方法

--- Widgets/FrmVideo/CameraOpencv.cpp
+++ Widgets/FrmVideo/CameraOpencv.cpp
@@ -41,32 +41,36 @@ void CCameraOpencv::slotTimeOut()
 {
     cv::Mat frame;
     m_videoCapture >> frame;
-    LOG_MODEL_DEBUG("CCameraOpencv", "frame.type:%d;format:%d", frame.type(), 
+    if(frame.empty())
+        return;
+    /*LOG_MODEL_DEBUG("CCameraOpencv", "frame.type:%d;format:%d", frame.type(), 
                 #ifdef ANDROID
                     m_videoCapture.get(cv::CAP_PROP_ANDROID_PREVIEW_FORMAT)
                 #else
                     m_videoCapture.get(cv::CAP_PROP_FORMAT)
                 #endif
                     );
-
+    */
+    
+    
+    //因为opencv会在内部把图像转化为BGR格式  
     cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);  
     cv::flip(frame, frame, 1);  
-    QImage image((uchar*)(frame.data), frame.cols, frame.rows, QImage::Format_RGB888);  //RGB888就是RGB24即RGB
-    QVideoFrame outFrame(image);
+    //*第一种转换方法：用QImage  
+    QImage image((uchar*)(frame.data), frame.cols, frame.rows, QImage::Format_RGB888);  //RGB888就是RGB24即RGB  
+    QVideoFrame outFrame(image);//*/
 
-    /*QByteArray outData((const char*)frame.data, (int)(frame.total() * frame.channels()));
-    //QByteArray outData;
-    //outData.append((const char*)frame.data, frame.total() * frame.channels());
-    //frame.total指图片像素个数，总字节数(dst.data)=dst.total*dst.channels() 
-    //outData.append(frame.data, frame.total() * frame.channels());
+    /*第二种转换方法：用CDataVideoBuffer  
+    QByteArray outData((const char*)frame.data, (int)(frame.total() * frame.channels()));
+    //frame.total指图片像素个数，总字节数(dst.data)=dst.total*dst.channels()  
     //由QVideoFrame进行释放  
     CDataVideoBuffer* pBuffer = new CDataVideoBuffer(outData,
-                            frame.rows,
-                            frame.cols);
+                            frame.cols,
+                            frame.rows);
     //LOG_MODEL_DEBUG("CCameraOpencv", "CCameraOpencv format:%d", dbFormat);
     QVideoFrame outFrame(pBuffer,
-                         QSize(frame.rows,
-                               frame.cols),
+                         QSize(frame.cols,
+                               frame.rows),
                          QVideoFrame::Format_RGB24);//*/
     
     emit sigCaptureFrame(outFrame);
