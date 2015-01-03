d47b8edad859f79480eb985557bd4965899f1712
KangLin <kl222@126.com>
2015-01-03 11:38:19 +0800

    增加cmake for android自动化编译 自动编译测试用 android ndk r10d

--- ThirdLibary/build_script/travis/build-and-test.sh
+++ ThirdLibary/build_script/travis/build-and-test.sh
@@ -2,23 +2,24 @@
 set -ev
 
 if [ -n "$ANDROID_TARGET" ]; then #编译 android
-    export ANDROID_NDK_ROOT=`pwd`/ThirdLibary/android-ndk-r9c
+    export ANDROID_NDK_ROOT=`pwd`/ThirdLibary/android-ndk
+    export ANDROID_NDK=$ANDROID_NDK_ROOT
     export CC=$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin/arm-linux-androideabi-gcc
     export CXX=$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin/arm-linux-androideabi-g++
 fi
 
-QMAKE_ARGS="QXMPP_USE_VPX=1 RABBITIM_USER_FFMPEG=1 RABBITIM_USER_LIBCURL=1 RABBITIM_USER_OPENSSL=1"
-
-if [ -n "$CC" ]; then
-    QMAKE_ARGS="$QMAKE_ARGS QMAKE_CC=$CC"
-fi
-
-if [ -n "$CXX" ]; then
-    QMAKE_ARGS="$QMAKE_ARGS QMAKE_CXX=$CXX"
-fi
-
 # compile
 if [ -n "$QMAKE" ]; then
+    QMAKE_ARGS="QXMPP_USE_VPX=1 RABBITIM_USER_FFMPEG=1 RABBITIM_USER_LIBCURL=1 RABBITIM_USER_OPENSSL=1"
+
+    if [ -n "$CC" ]; then
+        QMAKE_ARGS="$QMAKE_ARGS QMAKE_CC=$CC"
+    fi
+
+    if [ -n "$CXX" ]; then
+       QMAKE_ARGS="$QMAKE_ARGS QMAKE_CXX=$CXX"
+    fi
+
     echo "$QMAKE $QMAKE_ARGS"
     $QMAKE $QMAKE_ARGS
     make VERBOSE=1
@@ -26,6 +27,10 @@ fi
 
 #用cmake编译
 if [ -n "$CMAKE" ]; then
-    $CMAKE . -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DQt5_DIR=${Qt5_DIR} 
+    if [ -n "$ANDROID_TARGET" ]; then #android需要指定cmake_toolchain_file
+        CMAKE_ARGS="-DCMAKE_TOOLCHAIN_FILE=`pwd`/platforms/android/android.toolchain.cmake"
+    fi
+
+    $CMAKE . -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DQt5_DIR=${Qt5_DIR} "${CMAKE_ARGS}"
     $CMAKE  --build . --config Release
 fi
