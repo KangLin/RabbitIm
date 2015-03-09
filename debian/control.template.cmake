7a5b0b5fb7d7891c1bfb6dab74242b264cf03473
KangLin <kl222@126.com>
2015-03-09 16:33:46 +0800

    deb去掉源码包信息

--- debian/control.template.cmake
+++ debian/control.template.cmake
@@ -1,13 +1,3 @@
-Source: @CPACK_DEBIAN_PACKAGE_NAME@
-Section: x11
-Priority: @CPACK_DEBIAN_PACKAGE_PRIORITY@
-Maintainer: @CPACK_DEBIAN_PACKAGE_MAINTAINER@
-Build-Depends: debhelper (>= 8.0.0), cmake, @CPACK_DEBIAN_PACKAGE_DEPENDS@
-Standards-Version: @CPACK_DEBIAN_PACKAGE_VERSION@
-Homepage: https://github.com/KangLin/rabbitim
-Vcs-Git: git@github.com:KangLin/rabbitim.git
-Vcs-Browser: https://github.com/KangLin/rabbitim
-
 Package: @CPACK_DEBIAN_PACKAGE_NAME@
 Architecture: any
 Version: @CPACK_DEBIAN_PACKAGE_VERSION@
